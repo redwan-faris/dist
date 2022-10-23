641dfaefbb05f49e7c49e3f5daa030d99d441f09
Redwan <redwanfarisa@gmail.com>
2022-10-23 13:50:11 +0300

    accept certificate

--- messenger/entities/messenger.entity.js
+++ messenger/entities/messenger.entity.js
@@ -12,7 +12,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Messenger = void 0;
 const user_entity_1 = require("../../user/entities/user.entity");
 const typeorm_1 = require("typeorm");
-const class_transformer_1 = require("class-transformer");
 let Messenger = class Messenger {
 };
 __decorate([
@@ -25,7 +24,6 @@ __decorate([
 ], Messenger.prototype, "message", void 0);
 __decorate([
     (0, typeorm_1.ManyToOne)(type => user_entity_1.User, user => user.messeges),
-    (0, class_transformer_1.Transform)(({ value }) => value.phone),
     __metadata("design:type", user_entity_1.User)
 ], Messenger.prototype, "user", void 0);
 __decorate([
